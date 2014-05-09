252ae8a4adee2a2dbdcae604fc3f5dc40242fd0c
Lauciano <jose.lauciano@gmail.com>
2014-05-09 19:32:03 -0300

    Esqueleto do Main
    Main iniciado

---
+++ AnalisadorLexico/AnalisadorLexico/main.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+
+using namespace std;
+
+int main(int n_arg, char** args){
+	//Variáveis e Ponteiros
+	ifstream *code;
+	ofstream *lexc;
+	string *f_name = new string(args[1]),
+		*e_code = new string(".code"),
+		*e_lexc = new string(".lexc");
+
+	//Abertura de Arquivos
+	code = new ifstream((*f_name + *e_code).c_str());
+	lexc = new ofstream((*f_name + *e_lexc).c_str());
+
+	//Análise Léxica
+
+	//Fechamento de Arquivos
+	code->close;
+	lexc->close;
+
+	//Retorno
+	return 0;
+}
