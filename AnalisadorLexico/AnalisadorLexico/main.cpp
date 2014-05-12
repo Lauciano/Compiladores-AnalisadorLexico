173593fe9d88750f884ca0649ae74fc764052bbb
Jotasz <juliodaniel.carvalho@gmail.com>
2014-05-11 22:33:03 -0300

    Mudanças

--- AnalisadorLexico/AnalisadorLexico/main.cpp
+++ AnalisadorLexico/AnalisadorLexico/main.cpp
@@ -1,11 +1,28 @@
 #include <fstream>
 #include <vector>
+#include <stdio.h>
+#include <list>
 
 using namespace std;
+
+class lexToken
+{
+	int linha;
+	string nome;
+	string classificacao;
+
+public:
+	lexToken(int line, string name, string classif){
+		linha = line;
+		nome = name;
+		classificacao = classif;
+	}
+};
 
 int main(int n_arg, char** args){
 	//Variáveis e Ponteiros
 	ifstream *code;
+	char c;
 	ofstream *lexc;
 	string l_reserv[] = {"program", "var", "integer","real","boolean",
 		"procedure","begin","end","if", "then","else","while","do","not"};
@@ -13,13 +30,23 @@ int main(int n_arg, char** args){
 		*e_code = new string(".txt"),
 		*e_lexc = new string(".out");
 
+	std::list<lexToken> list;
+
 	//Abertura de Arquivos
-	code = new ifstream((*f_name + *e_code).c_str());
-	lexc = new ofstream((*f_name + *e_lexc).c_str());
+	//ISSO AQUI TEM ERRO LAU, CONSERTA AÍ. VOU ASSUMIR QUE CODE e LEXC ESTÃO FUNCIONANDO
+	//O meu intellisense tá dizendo aqui que o operador "+" não pode ser usado com esses operandos aí
+	//code = new ifstream((*f_name + *e_code).c_str());
+	//lexc = new ofstream((*f_name + *e_lexc).c_str());
+	//Vou fazer a escrita em C, depois se você quiser mudar beleza.
+	FILE *input, *output;
+	input = fopen((char*)(f_name),"r");
+	output = fopen((char*)(e_lexc),"w");
+	
 
 	//Análise Léxica
-
 	/*Inicio do autômato, estado 1*/
+	while((c = fgetc(input)) =! EOF){
+
 
 
 
